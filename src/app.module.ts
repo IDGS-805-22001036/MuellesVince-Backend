ae264d0b2b2d0e0aa5da924013f64c06a7cee361
IDGS-805-22001036 <80107@alumnos.utleon.edu.mx>
2026-03-17 14:14:24 -0600

    Cambio de module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,14 +17,12 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: false,
-
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
 
     UsuariosModule,
