dd46172d146d630a95f217fb9506a9a4e4a6a06c
IDGS-805-22001036 <80107@alumnos.utleon.edu.mx>
2026-04-07 20:12:50 -0600

    Configuraciones de arranque v2

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8081;
   await app.listen(port, '0.0.0.0');
 
   console.log(`Servidor corriendo en el puerto ${port}`);
